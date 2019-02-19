b1b57e51befe1679af6b863a702a930d9b54afc5
justinpu <43174215+justinpu@users.noreply.github.com>
2019-02-19 20:47:01 +0800

    Add files via upload

---
+++ w01-2 variable-var-let-const/app.js
@@ -0,0 +1,36 @@
+var name = "chang pu"
+console.log(name);
+name='張樸';
+console.log(name);
+name='2019';
+console.log(name);
+
+var greeting;
+console.log(greeting);
+greeting='hello';
+console.log(greeting);
+
+var firstName='chang';
+var first_name='john';
+var FirstName='sara';
+var firstname;
+
+let name2="chang pu";
+console.log(name2);
+name2='張樸';
+console.log(name2);
+name2="2019";
+console.log(name2);
+
+const person = {
+    name:'Pu',
+    age:20
+};
+
+person.name='chang pu';
+person.age = 20;
+console.log(person);
+
+const numbers=[6,9,6,9];
+numbers.push(6);
+console.log(numbers);
---
+++ w01-3/app.js
@@ -0,0 +1,24 @@
+const name='Pu';
+console.log(typeof name);
+
+const age=20;
+console.log(typeof age);
+
+const haskids=true;
+console.log(typeof haskids);
+
+const car=null;
+console.log(typeof car);
+
+let test;
+console.log(typeof test);
+
+const sym=Symbol();
+console.log(typeof sym);
+
+const hobbies=['movies','music'];
+console.log(typeof hobbies);
+
+const today =new Date();
+console.log(today);
+console.log(typeof today);
