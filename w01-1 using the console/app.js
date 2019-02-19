a9587989c492eeebcc9f98ffd27a4fc63e5cd2f3
justinpu <43174215+justinpu@users.noreply.github.com>
2019-02-19 19:18:10 +0800

    Add files via upload

---
+++ w01-1 using the console/app.js
@@ -0,0 +1,22 @@
+console.log('hellow word');
+console.log(1234); //207410035
+console.log(true);
+
+var greeting = 'chang'; //張樸
+console.log(greeting);
+console.log([1,2,3,4])//2019/02/19
+console.log({firstname:'chang',lastname:'Pu'});
+console.table({firstname:'chang',lastname:'Pu'});
+
+console.error('this is erro');
+console.warn('this is some warning');
+
+console.time('hello');
+console.log('hellow word');
+console.log('hellow word');
+console.log('hellow word');
+console.log('hellow word');
+console.log('hellow word');
+console.log('hellow word');
+
+console.timeEnd('hello');
